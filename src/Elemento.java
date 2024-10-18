e19a9d54b4d92b29530080c380fa2358c01e0cbd
GiuLou <giulialouzzada@gmail.com>
2024-10-17 23:33:18 -0300

    web

---
+++ src/Elemento.java
@@ -0,0 +1,8 @@
+public class Elemento {
+    Web pagina;
+    Elemento proximo;
+    public Elemento(Web pagina) {
+        this.pagina = pagina;
+        this.proximo = null;
+    }
+}
---
+++ src/Pilha.java
@@ -0,0 +1,45 @@
+public class Pilha {
+    Elemento topo;
+    int tam;
+    public Pilha() {
+        this.topo = null;
+        this.tam = 0;
+    }
+    public void push(Web pagina) {
+        Elemento e = new Elemento(pagina);
+        e.proximo = topo;
+        topo = e;
+        tam++;
+    }
+    public Web pop() {
+        if (empty()) {
+            System.out.println("Pilha vazia");
+            return null;
+        }
+        Elemento e = topo;
+        topo = topo.proximo;
+        tam--;
+        return e.pagina;
+    }
+    public int size() {
+        return tam;
+    }
+    public boolean empty() {
+        return tam == 0;
+    }
+    public void imprimir() {
+        if (empty()) {
+            System.out.println("Pilha vazia");
+            return;
+        }
+        Elemento atual = topo;
+        while (atual != null) {
+            System.out.println(atual.pagina);
+            if (atual.proximo != null) {
+                System.out.println(">");
+            }
+            atual = atual.proximo;
+        }
+        System.out.println();
+    }
+}
